986ef8ccb419f3eba5dacee16cf76e1e18024ac2
chanqun <cksgns93@naver.com>
2022-05-22 22:17:51 +0900

    variable and constants

--- main.go
+++ main.go
@@ -5,6 +5,19 @@ import (
 )
 
 func main() {
-	fmt.Println("Hello world!") //formatting package
+	// 1
+	fmt.Println("Hello world!") // formatting package
 
+	// 2
+	const name string = "chanqun" // go는 type 언어
+	const flag bool = true
+
+	var changeName string = "chanqun"
+	changeName = "cq"
+	otherName := "sung"
+
+	fmt.Println(changeName)
+	fmt.Println(otherName)
+
+	// 3
 }
