3f401dd372a9d9e76a700278eac90adf0964a4e2
chanqun <cksgns93@naver.com>
2022-05-28 14:46:50 +0900

    동기 방식

--- main.go
+++ main.go
@@ -1,18 +1,54 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"github.com/chanqun/go-example/mydict"
+	http "net/http"
 )
 
 func main() {
-	dictionary := mydict.Dictionary{"first": "First word"}
-
-	definition, err := dictionary.Search("second")
-
-	if err != nil {
-		fmt.Println(err)
+	urls := []string{
+		"https://www.google.com",
+		"https://www.naver.com",
+		"https://www.yahoo.com",
+		"https://www.airbnb.com/",
+		"https://www.google.com/",
+		"https://www.amazon.com/",
+		"https://www.reddit.com/",
+		"https://soundcloud.com/",
+		"https://www.facebook.com/",
+		"https://www.instagram.com/",
+		"https://academy.nomadcoders.co/",
 	}
 
-	fmt.Println(definition)
+	var results = make(map[string]string)
+
+	for _, url := range urls {
+		err := hitUrl(url)
+
+		var result = "OK"
+		if err != nil {
+			result = "FAILED"
+		}
+
+		results[url] = result
+	}
+
+	for url, result := range results {
+		fmt.Println(url, result)
+	}
+}
+
+var errRequestFailed = errors.New("request failed")
+
+func hitUrl(url string) error {
+	fmt.Println("Checking", url)
+
+	resp, err := http.Get(url)
+
+	if err != nil || resp.StatusCode >= 400 {
+		return errRequestFailed
+	}
+
+	return nil
 }
