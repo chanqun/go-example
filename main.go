ea5c8bfc0679650c48e59caa05c6dd9e7918db61
chanqun <cksgns93@naver.com>
2022-05-31 23:34:43 +0900

    scrapper

--- main.go
+++ main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
-	"fmt"
-	"time"
+	"github.com/chanqun/go-example/scrapper"
+	"os"
+	"strings"
+
+	"github.com/labstack/echo"
 )
 
-func main() {
-	c := make(chan bool)
-	people := [2]string{"chanqun", "sungmin"}
+const fileName string = "jobs.csv"
 
-	for _, person := range people {
-		go isSexy(person, c)
-	}
-
-	result := <-c
-	result2 := <-c
-
-	fmt.Println(result, result2)
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
 }
 
-func isSexy(person string, c chan bool) {
-	time.Sleep(time.Second * 3)
-	c <- true
+func handleScrape(c echo.Context) error {
+	defer os.Remove("jobs.csv")
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment("jobs.csv", "jobs.csv")
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
 }
