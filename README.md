a9be6bcad80ce60f030dbdf743a10fdcb294ed56
chanqun <cksgns93@naver.com>
2022-06-13 22:48:08 +0900

    csv 읽기

--- README.md
+++ README.md
@@ -308,3 +308,38 @@ func isSexy(person string, c chan bool) {
 	c <- true
 }
 ```
+
+
+최종 메인 코드
+
+```go
+package main
+
+import (
+	"github.com/chanqun/go-example/scrapper"
+	"os"
+	"strings"
+
+	"github.com/labstack/echo"
+)
+
+const fileName string = "jobs.csv"
+
+func handleHome(c echo.Context) error {
+	return c.File("home.html")
+}
+
+func handleScrape(c echo.Context) error {
+	defer os.Remove("jobs.csv")
+	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
+	scrapper.Scrape(term)
+	return c.Attachment("jobs.csv", "jobs.csv")
+}
+
+func main() {
+	e := echo.New()
+	e.GET("/", handleHome)
+	e.POST("/scrape", handleScrape)
+	e.Logger.Fatal(e.Start(":1323"))
+}
+```
--- main.go
+++ main.go
@@ -1,29 +1,30 @@
 package main
 
 import (
-	"github.com/chanqun/go-example/scrapper"
+	"bufio"
+	"encoding/csv"
+	"fmt"
 	"os"
-	"strings"
-
-	"github.com/labstack/echo"
 )
 
-const fileName string = "jobs.csv"
-
-func handleHome(c echo.Context) error {
-	return c.File("home.html")
-}
-
-func handleScrape(c echo.Context) error {
-	defer os.Remove("jobs.csv")
-	term := strings.ToLower(scrapper.CleanString(c.FormValue("term")))
-	scrapper.Scrape(term)
-	return c.Attachment("jobs.csv", "jobs.csv")
-}
-
 func main() {
-	e := echo.New()
-	e.GET("/", handleHome)
-	e.POST("/scrape", handleScrape)
-	e.Logger.Fatal(e.Start(":1323"))
+	file, err := os.Open("day/000120.csv")
+
+	if err != nil {
+		panic(err)
+	}
+
+	// csv reader 생성
+	rdr := csv.NewReader(bufio.NewReader(file))
+
+	// csv 내용 모두 읽기
+	rows, _ := rdr.ReadAll()
+
+	// 행,열 읽기
+	for i, row := range rows {
+		for j := range row {
+			fmt.Printf("%s ", rows[i][j])
+		}
+		fmt.Println()
+	}
 }
